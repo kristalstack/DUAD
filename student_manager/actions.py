cdef26962a797e751509ac87f1ebaaac66507761
Kristal Pastor <kristalpastor@gmail.com>
2026-03-21 12:47:13 -0600

    Add student manager module project

---
+++ student_manager/actions.py
@@ -0,0 +1,104 @@
+def get_valid_grade(subject_name):
+    while True:
+        try:
+            grade = float(input(f"Enter {subject_name} grade (0-100): "))
+            
+            if 0 <= grade <= 100:
+                return grade
+            else:
+                print("Invalid grade. Must be between 0 and 100.")
+        
+        except ValueError:
+            print("Invalid input. Enter a number.")
+
+def calculate_student_average(student):
+    return (
+        student["spanish"] +
+        student["english"] +
+        student["social_studies"] +
+        student["science"]
+    ) / 4
+
+def add_students(students):
+    while True:
+        try:
+            amount = int(input("How many students do you want to add? "))
+            if amount > 0:
+                break
+            else:
+                print("Enter a number greater than 0.")
+        except ValueError:
+            print("Invalid input.")
+
+    for i in range(amount):
+        print(f"\nStudent #{i + 1}")
+
+        full_name = input("Full name: ")
+        section = input("Section: ")
+
+        spanish = get_valid_grade("Spanish")
+        english = get_valid_grade("English")
+        social_studies = get_valid_grade("Social Studies")
+        science = get_valid_grade("Science")
+
+        student = {
+            "full_name": full_name,
+            "section": section,
+            "spanish": spanish,
+            "english": english,
+            "social_studies": social_studies,
+            "science": science
+        }
+
+        students.append(student)
+
+    print("\nStudents added successfully.")
+
+def show_all_students(students):
+    if not students:
+        print("\nNo students registered.")
+        return
+
+    for i, student in enumerate(students, start=1):
+        avg = calculate_student_average(student)
+
+        print(f"\nStudent #{i}")
+        print(f"Name: {student['full_name']}")
+        print(f"Section: {student['section']}")
+        print(f"Spanish: {student['spanish']}")
+        print(f"English: {student['english']}")
+        print(f"Social Studies: {student['social_studies']}")
+        print(f"Science: {student['science']}")
+        print(f"Average: {avg:.2f}")
+
+def show_top_3_students(students):
+    if not students:
+        print("\nNo students registered.")
+        return
+
+    sorted_students = sorted(
+        students,
+        key=calculate_student_average,
+        reverse=True
+    )
+
+    top_students = sorted_students[:3]
+
+    print("\nTop 3 students:")
+    for i, student in enumerate(top_students, start=1):
+        avg = calculate_student_average(student)
+        print(f"{i}. {student['full_name']} - {avg:.2f}")
+
+def show_average_of_all_students(students):
+    if not students:
+        print("\nNo students registered.")
+        return
+
+    total = 0
+
+    for student in students:
+        total += calculate_student_average(student)
+
+    average = total / len(students)
+
+    print(f"\nGeneral average: {average:.2f}")
---
+++ student_manager/main.py
@@ -0,0 +1,41 @@
+from menu import show_menu, get_menu_option
+from actions import (
+    add_students,
+    show_all_students,
+    show_top_3_students,
+    show_average_of_all_students
+)
+from data import export_to_csv, import_from_csv
+
+def main():
+    students = []
+
+    while True:
+        show_menu()
+        option = get_menu_option()
+
+        if option == "1":
+            add_students(students)
+
+        elif option == "2":
+            show_all_students(students)
+
+        elif option == "3":
+            show_top_3_students(students)
+
+        elif option == "4":
+            show_average_of_all_students(students)
+
+        elif option == "5":
+            export_to_csv(students)
+
+        elif option == "6":
+            students = import_from_csv()
+
+        elif option == "7":
+            print("\nGoodbye!")
+            break
+
+
+if __name__ == "__main__":
+    main()
